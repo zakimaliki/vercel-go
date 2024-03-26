bea3f62a68eac815f14951a74fcf2a364e7a7b0c
malikizaki94 <malikizakitop@gmail.com>
2024-03-26 22:17:13 +0700

    fc

--- api/index.go
+++ api/index.go
@@ -7,6 +7,19 @@ import (
 	. "github.com/tbxark/g4vercel"
 )
 
+type Product struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Stock int    `json:"stock"`
+}
+
+var products = []Product{
+	{Name: "Product A", Price: 100000, Stock: 100},
+	{Name: "Product B", Price: 140000, Stock: 50},
+	{Name: "Product C", Price: 123000, Stock: 200},
+	// Add more sample products as needed
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
 	server.Use(Recovery(func(err interface{}, c *Context) {
@@ -21,11 +34,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			})
 		}
 	}))
+
 	server.GET("/", func(context *Context) {
 		context.JSON(200, H{
 			"message": "OK",
 		})
 	})
+
 	server.GET("/hello", func(context *Context) {
 		name := context.Query("name")
 		if name == "" {
@@ -38,19 +53,32 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			})
 		}
 	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
+
+	server.GET("/products", func(context *Context) {
 		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
+			"data": products,
 		})
 	})
+
+	server.GET("/product/:id", func(context *Context) {
+		id := context.Param("id")
+		index := -1
+		for i, product := range products {
+			if product.Name == id {
+				index = i
+				break
+			}
+		}
+		if index == -1 {
+			context.JSON(404, H{
+				"message": "Product not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": products[index],
+			})
+		}
+	})
+
 	server.Handle(w, r)
 }
